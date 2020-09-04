80f8d1b2154b3ca020e92b2eccbcfb61538cec47
vinsdragonis <37930821+vinsdragonis@users.noreply.github.com>
2020-09-05 03:37:36 +0530

    Update MatrixDisplayEMO.ino

--- MatrixDisplayEMO.ino
+++ MatrixDisplayEMO.ino
@@ -8,14 +8,12 @@ LedControl lc = LedControl (DIN,CLK,CS,0);
 int x = 1;
 
 void setup() {
-  // put your setup code here, to run once:
   lc.shutdown(0,false);
   lc.setIntensity(0,x);
   lc.clearDisplay(0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   byte emo[8] = {B00111100,B01000010,B10100101,B10000001,B10100101,B10011001,B01000010,B00111100};
   byte hi[8] = {0x00,0xAE,0xA4,0xE4,0xA4,0xAE,0x00,0x00};
   byte right[8] = {B00000000,B00000100,B00000110,B11111111,B00000110,B00000100,B00000000,B00000000};
@@ -26,14 +24,6 @@ void loop() {
   lc.setIntensity(0,5);
   printByte(emo);
   delay(2000);
-
-  /*for (float i=0;i<=15;i+=.5)
-  {
-    lc.setIntensity(0,i);
-    printByte(hi);
-    delay(10);
-  }
-  delay(2500);*/
 
   printByte(right);
   delay(2500);
